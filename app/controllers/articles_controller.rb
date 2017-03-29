0087d6297dd409e14cdae23439d81f47657878c3
ehabzaki <m.ehabzaki@gmail.com>
2017-03-29 14:49:00 -0400

    add login for user

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -25,6 +25,8 @@ class ArticlesController < ApplicationController
   # POST /articles.json
   def create
     @article = Article.new(article_params)
+        @article.user_id = session[:user].to_i
+
 
     respond_to do |format|
       if @article.save
--- app/controllers/controls_controller.rb
+++ app/controllers/controls_controller.rb
@@ -1,4 +1,17 @@
 class ControlsController < ApplicationController
   def login
-  end
+      if request.post?
+          user=User.find_by(:email=>params[:email],:password=>params[:password])
+          if !user.nil?
+              session[:user]=user.id
+              redirect_to root_url,notice:  "Thank you for signing up"
+          else 
+              respond_to do |format|
+                  format.html{render:new}
+                  
+        end
+    end
+
+    end
+    end
 end
